ffe26ce84a08d669b0f5cd534436a7cf72999cf7
Jonathan <jmakatk@gmail.com>
2024-04-18 16:11:14 -0600

    fix: loading images for web in release mode

--- lib/three3d/loaders/image_loader_for_web.dart
+++ lib/three3d/loaders/image_loader_for_web.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
-import 'package:three_dart/extra/blob.dart';
 import 'dart:html' as html;
+import 'dart:typed_data';
+import '../../extra/blob.dart';
 
 class ImageLoaderLoader {
   // flipY 在web环境下 忽略
   // in web the flipping is done when rendering the texture, rather than when loading the texture
-  static Future<html.ImageElement> loadImage(url, flipY, {Function? imageDecoder}) {
+  static Future<html.ImageElement> loadImage(url, flipY,
+      {Function? imageDecoder}) {
     var completer = Completer<html.ImageElement>();
     var imageDom = html.ImageElement();
     imageDom.crossOrigin = "";
@@ -14,16 +16,17 @@ class ImageLoaderLoader {
       completer.complete(imageDom);
     });
 
-    if (url is Blob) {
-      var blob = html.Blob([url.data.buffer], url.options["type"]);
-      imageDom.src = html.Url.createObjectUrl(blob);
-    } else {
-      // flutter web for assets need add assets TODO
+    if (url is String) {
       if (url.startsWith("assets")) {
         imageDom.src = "assets/$url";
       } else {
         imageDom.src = url;
       }
+    } else {
+      Blob clone = Blob((url.data as Uint8List), {"type": "image/png"});
+
+      var blob = html.Blob([clone.data.buffer], clone.options["type"]);
+      imageDom.src = html.Url.createObjectUrl(blob);
     }
 
     return completer.future;
